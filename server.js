204fd8c024afb5b748014bc1f1f04fbddf3d7c65
Neer Patel <neerpatel@gmail.com>
2023-12-28 21:17:23 -0600

    Fixed issues syntax issues

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 'use strict';
 
 const express = require('express');
-function executeCmd(cmd,const logger = require("./config/winston"); callback) {
+const logger = require("./config/winston");
 
 function executeCmd(cmd, callback) {
 
-    exec(cmd, function (err, stdout, stderror) {
-      // console.log('stdout: %s', output);
-      if (err) {
-        callback(err);
-      } 
-      else if (stderror) {
-        callback(stderror);
+  exec(cmd, function (err, stdout, stderror) {
+    // console.log('stdout: %s', output);
+    if (err) {
+      callback(err);
+    }
+    else if (stderror) {
+      callback(stderror);
+    }
+    else {
+      if (stdout) {
+        callback(null, stdout);
       }
       else {
-        if (stdout) {
-          callback(null,stdout);
-        } 
-        else {
-          callback(null, null);
-        }
+        callback(null, null);
       }
-    });
+    }
+  });
 }
 
 
@@ -31,38 +31,42 @@ const app = express();
 app.get('/', (req, res) => {
 
   var wanted = ['upsname', 'serialno', 'status', 'linev', 'linefreq', 'loadpct', 'battv', 'bcharge'];
-
-  executeCmd('apcaccess', function(err, response) {
-    if (err) {
-      console.error(err);
-    }
-    else {
-      logger.info(response);
-      var lines = response.trim().split("\n");
+  try {
+    executeCmd('apcaccess', function (err, response) {
       var output = {};
-      // loop over every line
-      lines.forEach(function (line) {
-        var lineData = [];
-        // assign values
-        var stats = line.split(' : ');
-        var label = stats[0].toLowerCase();
-        var value = stats[1];
+      if (err) {
+        throw err;
+      }
+      else {
+        logger.info(response);
+        var lines = response.trim().split("\n");
+        // loop over every line
+        lines.forEach(function (line) {
+          var lineData = [];
+          // assign values
+          var stats = line.split(' : ');
+          var label = stats[0].toLowerCase();
+          var value = stats[1];
 
-        // remove surrounding spaces
-        label = label.replace(/(^\s+|\s+$)/g, '');
-        // if found as wanted value, store it
-        if (wanted.indexOf(label) > -1) {
-          value = value.replace(/(^\s+|\s+$)/g, '');
+          // remove surrounding spaces
+          label = label.replace(/(^\s+|\s+$)/g, '');
+          // if found as wanted value, store it
+          if (wanted.indexOf(label) > -1) {
+            value = value.replace(/(^\s+|\s+$)/g, '');
 
             output[label] = value;
             //output.push(lineData);
             if (err) throw err;
-        }
-      });
-      res.send(JSON.stringify(output));
-    }
-  })
-	
+          }
+        });
+        res.status(200).json(JSON.stringify(output));
+      }
+    })
+  } catch (error) {
+    logger.error(error);
+    res.status(500).json(JSON.stringify(error));
+  }
+
 });
 
 const port = process.env.PORT || PORT;
