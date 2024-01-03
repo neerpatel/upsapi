5753e14ecb618eecc61ceb0fbf5ae6814e5176c6
Neer Patel <neerpatel@gmail.com>
2024-01-03 12:48:16 -0600

    updated deployment

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
     watch: '.',
     env_production: {
       NODE_ENV: "production",
-      PORT: 80,
-      HUB_IP: "",
+      PORT: 8070,
+      HUB_IP: "192.168.107.116",
       HUB_PORT: 39501,
       APCNIS_IP: "127.0.0.1",
       APCNIS_PORT: 3551
@@ -15,7 +15,7 @@ module.exports = {
    env_development: {
       NODE_ENV: "development",
       PORT: 8070,
-      HUB_IP: "",
+      HUB_IP: "192.168.107.116",
       HUB_PORT: 39501,
       APCNIS_IP: "127.0.0.1",
       APCNIS_PORT: 3551
@@ -28,10 +28,9 @@ module.exports = {
       host : '127.0.0.1',
       ref  : 'origin/main',
       repo : 'https://github.com/neerpatel/upsapi',
-      path : '/var/node/upsapi',
+      path : '/opt/upsapi',
       'pre-deploy-local': '',
-      'post-deploy' : 'pm2 startOrRestart ecosystem.config.js --env production',
-      'pre-setup': 'npm install'
+      'post-deploy': 'npm install'
     }
   }
 };
