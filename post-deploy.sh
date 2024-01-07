63aa5fc3beb6786f080b7618e99a89c8b7dc6a39
Neer Patel <neerpatel@gmail.com>
2024-01-07 01:33:23 -0600

    Updated setup script to change UPS name

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,3 +1,10 @@
+if [ ! -f .env ] then
+  export $(cat .env | xargs)
+fi
 sudo cp -rf ./apcupsd-scripts/* /etc/apcupsd
+
+sudo sed -E 's/__(([^_]|_[^_])*)__/${\1}/g' /etc/apcupsd/apcupsd.conf |envsubst > /etc/apcupsd/apcupsd.conf
+sudo /etc/init.d/apcupsd start
+
 cd ./app && npm install
 cd ..
--- setup.sh
+++ setup.sh
@@ -34,3 +34,13 @@ if [ "$(stat -c '%U:%G' /opt/upsapi)" != "pi:pi" ]; then
     sudo chown -R pi:pi /opt/upsapi
 fi
 
+curl -sL https://raw.githubusercontent.com/neerpatel/upsapi/main/app/ecosystem.config.js -O 
+pm2 deploy production setup 
+
+echo "What is the name of your UPS?"
+printf 'What is the name of your UPS?\n' "$UPSNAME" >.env
+
+
+
+cd /opt/upsapi/app/
+pm2 deploy production
