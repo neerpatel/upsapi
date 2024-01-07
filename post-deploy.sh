7b4642468bf7209d697590d3319a39edc820537a
Neer Patel <neerpatel@gmail.com>
2024-01-07 02:01:53 -0600

    Updated setup script to change UPS name

--- post-deploy.sh
+++ post-deploy.sh
@@ -2,9 +2,9 @@ if [ ! -f .env ]; then
   export $(cat .env | xargs)
 fi
 
-sed -E 's/__(([^_]|_[^_])*)__/${\1}/g' ./apcupsd-scripts/apcupsd.conf |envsubst > ./apcupsd-scripts/apcupsd.conf
+sed -E 's/__(([^_]|_[^_])*)__/${\1}/g' ./apcupsd-scripts/apcupsd.conf.tpm |envsubst > ./apcupsd-scripts/apcupsd.conf
 sudo cp -rf ./apcupsd-scripts/* /etc/apcupsd
-git restore ./apcupsd-scripts/apcupsd.conf
+#rm ./apcupsd-scripts/apcupsd.conf
 
 sudo /etc/init.d/apcupsd start
 
