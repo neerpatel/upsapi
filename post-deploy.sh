0ca2373824da44048ade5c4c3305b19bf8854dc2
Neer Patel <neerpatel@gmail.com>
2024-01-07 12:55:54 -0600

    Updated setup script to change UPS name

--- post-deploy.sh
+++ post-deploy.sh
@@ -3,10 +3,16 @@ if [ ! -f .env ]; then
 fi
 
 sed -E 's/__(([^_]|_[^_])*)__/${\1}/g' ./apcupsd-scripts/apcupsd.conf.tpl |envsubst > ./apcupsd-scripts/apcupsd.conf
-sudo cp -rf ./apcupsd-scripts/* /etc/apcupsd
-#rm ./apcupsd-scripts/apcupsd.conf
 
-sudo /etc/init.d/apcupsd start
+echo "Stopping apcupsd service"
+sudo systemctl stop apcupsd.service
 
+echo "Copying apcupsd-scripts to /etc/apcupsd/"
+sudo cp -rfv ./apcupsd-scripts/ /etc/apcupsd
+
+echo "Stopping apcupsd service"
+sudo systemctl start apcupsd.service
+
+echo "Running npm install"
 cd ./app && npm install
 cd ..
